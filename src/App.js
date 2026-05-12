f69f5cc3d763006753d20005dc180ee1b21f9f18
Prathamesh Hukkeri <prathamesh04@users.noreply.github.com>
2026-05-12 15:38:04 +0530

    Initial React test job app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import { useEffect, useState } from "react";
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="page">
+      <h1>Users List 🚀</h1>
+
+      <div className="card-container">
+        {users.map((user) => (
+          <div className="card" key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <p>{user.company.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="page">
+      <h1>About Page</h1>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div className="page">
+      <h1>Contact Page</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <nav className="navbar">
+          <div className="logo">MyApp</div>
+
+          <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
